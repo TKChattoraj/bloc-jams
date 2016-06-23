2f9e8a26462ff9ae830e374b9ad759d5c3359d7e
Tarun Chattoraj <kumar.chattoraj@gmail.com>
2016-06-22 23:20:33 -0400

    completing checkpoint-23

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,7 @@
-var animatePoints = function() {
-            
-    var points = document.getElementsByClassName('point');
-    
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
+           
     var revealPoint = function(i) {
                 points[i].style.opacity = 1;
                 points[i].style.transform = "scaleX(1) translateY(0)";
@@ -12,14 +12,22 @@ var animatePoints = function() {
        revealPoint(i);
    }
 };
-var changeColor = function() {
-    var elementToChange = document.getElementsByClassName("hero-title");
-    console.log(elementToChange);
-    elementToChange[0].style.color = "blue";
+
+
+
+window.onload = function() {
+
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
     
-};
-
-
-animatePoints();
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    window.addEventListener('scroll', function(event) {
+      if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+          animatePoints(pointsArray);
+      } ;
+    });
+}
         
         
